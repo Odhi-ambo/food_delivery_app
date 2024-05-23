8bd1bad0735901ba614082d6c5c9ea7dbb907b94
Odhi-ambo <michaelderrick605@gmail.com>
2024-05-23 10:45:55 +0300

    initial commit

---
+++ lib/components/my_drawer.dart
@@ -0,0 +1,50 @@
+import 'package:delivery_app/components/my_drawer_tile.dart';
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 100.0),
+            child: Icon(
+              Icons.lock_open_rounded,
+              size: 80,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Divider(
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+          ),
+          MyDrawerTile(
+            text: 'HOME',
+            icon: Icons.home,
+            onTap: () {},
+          ),
+          MyDrawerTile(
+            text: 'SETTINGS',
+            icon: Icons.settings,
+            onTap: () {},
+          ),
+          const Spacer(),
+          MyDrawerTile(
+            text: 'LOGOUT',
+            icon: Icons.logout,
+            onTap: () {},
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+        ],
+      ),
+    );
+  }
+}
