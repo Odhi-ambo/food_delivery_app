0cf4df86d54bfd3f342b6a4018ec2aa885796b73
Odhi-ambo <michaelderrick605@gmail.com>
2024-05-24 17:15:21 +0300

    initial commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,11 @@
+import 'package:delivery_app/components/my_current_location.dart';
+import 'package:delivery_app/components/my_description_box.dart';
 import 'package:delivery_app/components/my_drawer.dart';
+import 'package:delivery_app/components/my_sliver_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -12,11 +15,29 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Center(child: Text('Home')),
-        backgroundColor: Theme.of(context).colorScheme.background,
-      ),
       drawer: const MyDrawer(),
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          MySliverAppBar(
+            title: const Text('title'),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Divider(
+                  indent: 25,
+                  endIndent: 25,
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+                const MyCurrentLocation(),
+                const MyDescriptionBox(),
+              ],
+            ),
+          ),
+        ],
+        body: Container(
+          color: Colors.amber,
+        ),
+      ),
     );
   }
 }
