56b7b34c57257ed531f522603e688f2c8eb25239
Odhi-ambo <michaelderrick605@gmail.com>
2024-05-27 14:42:27 +0300

    initial commit

--- lib/components/my_description_box.dart
+++ lib/components/my_description_box.dart
@@ -1,30 +1,48 @@
 import 'package:flutter/material.dart';
 
 class MyDescriptionBox extends StatelessWidget {
-  const MyDescriptionBox({super.key});
+  const MyDescriptionBox({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    var myPrimaryTextStyle =
+        TextStyle(color: Theme.of(context).colorScheme.onBackground);
+
+    var mySecondaryTextStyle =
+        TextStyle(color: Theme.of(context).colorScheme.primary);
+
     return Container(
       decoration: BoxDecoration(
         color: Theme.of(context).colorScheme.background,
         borderRadius: BorderRadius.circular(12),
       ),
-      child: const Padding(
-        padding: EdgeInsets.all(25.0),
+      child: Padding(
+        padding: const EdgeInsets.all(25.0),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Column(
               children: [
-                Text('kshs 50'),
-                Text('Delivery Fee'),
+                Text(
+                  'kshs 50',
+                  style: myPrimaryTextStyle,
+                ),
+                Text(
+                  'Delivery Fee',
+                  style: mySecondaryTextStyle,
+                ),
               ],
             ),
             Column(
               children: [
-                Text('10-15 min'),
-                Text('Delivery time'),
+                Text(
+                  '10-15 min',
+                  style: myPrimaryTextStyle,
+                ),
+                Text(
+                  'Delivery time',
+                  style: mySecondaryTextStyle,
+                ),
               ],
             ),
           ],
