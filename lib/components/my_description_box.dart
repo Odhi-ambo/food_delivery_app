cffe8d6f36acf611cdaf4c8660421698cb830e93
Odhi-ambo <michaelderrick605@gmail.com>
2024-05-27 15:13:30 +0300

    initial commit

--- lib/components/my_description_box.dart
+++ lib/components/my_description_box.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class MyDescriptionBox extends StatelessWidget {
-  const MyDescriptionBox({Key? key}) : super(key: key);
+  const MyDescriptionBox({super.key});
 
   @override
   Widget build(BuildContext context) {
