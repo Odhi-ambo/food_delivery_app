a688be1437dd684ef4f5a319aa0c8d6f7841936c
Odhi-ambo <michaelderrick605@gmail.com>
2024-05-28 20:41:46 +0300

    initial commit

--- lib/components/my_tab_bar.dart
+++ lib/components/my_tab_bar.dart
@@ -1,24 +1,22 @@
+import 'package:delivery_app/model/food.dart';
 import 'package:flutter/material.dart';
 
 class MyTabBar extends StatelessWidget {
   final TabController tabController;
   const MyTabBar({super.key, required this.tabController});
+  List<Tab> _buildCategoryTabs() {
+    return FoodCategory.values
+        .map((category) => Tab(text: category.toString().split('.').last))
+        .toList();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return TabBar(
-      controller: tabController,
-      tabs: const [
-        Tab(
-          icon: Icon(Icons.home),
-        ),
-        Tab(
-          icon: Icon(Icons.settings),
-        ),
-        Tab(
-          icon: Icon(Icons.person),
-        ),
-      ],
+    return Container(
+      child: TabBar(
+        controller: tabController,
+        tabs: _buildCategoryTabs(),
+      ),
     );
   }
 }
