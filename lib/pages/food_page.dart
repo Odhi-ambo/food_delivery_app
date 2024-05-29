6e33199f723d014c2f9a784047933f066db59ba5
Odhi-ambo <michaelderrick605@gmail.com>
2024-05-29 12:29:54 +0300

    initial commit

---
+++ lib/pages/food_page.dart
@@ -0,0 +1,19 @@
+import 'package:delivery_app/model/food.dart';
+import 'package:flutter/material.dart';
+
+class FoodPage extends StatefulWidget {
+  final Food food;
+  const FoodPage({super.key, required this.food});
+
+  @override
+  State<FoodPage> createState() => _FoodPageState();
+}
+
+class _FoodPageState extends State<FoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [Image.asset(widget.food.imagePath)],
+    );
+  }
+}
