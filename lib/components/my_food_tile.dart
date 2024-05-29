ff584e3c60892c8ce4f9a704ea2e912d79d98e7c
Odhi-ambo <michaelderrick605@gmail.com>
2024-05-29 12:28:46 +0300

    initial commit

---
+++ lib/components/my_food_tile.dart
@@ -0,0 +1,64 @@
+import 'package:delivery_app/model/food.dart';
+import 'package:flutter/material.dart';
+
+class FoodTile extends StatelessWidget {
+  final Food food;
+  final void Function()? onTap;
+  const FoodTile({super.key, required this.food, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        GestureDetector(
+          onTap: onTap,
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(food.name),
+                      Text(
+                        food.description,
+                        style: TextStyle(
+                            color:
+                                Theme.of(context).colorScheme.inversePrimary),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'kshs ${food.price}',
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  width: 15,
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Image.asset(
+                    food.imagePath,
+                    height: 120,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Divider(
+          color: Theme.of(context).colorScheme.tertiary,
+          endIndent: 25,
+          indent: 25,
+        )
+      ],
+    );
+  }
+}
