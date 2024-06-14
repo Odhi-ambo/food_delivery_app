8ee336e1fb6c6ad195164dbeb896f6b090a1b2aa
Odhi-ambo <michaelderrick605@gmail.com>
2024-06-14 11:11:04 +0300

    initial commit

--- lib/pages/food_page.dart
+++ lib/pages/food_page.dart
@@ -1,3 +1,4 @@
+import 'package:delivery_app/components/my_button.dart';
 import 'package:delivery_app/model/food.dart';
 import 'package:flutter/material.dart';
 
@@ -13,10 +14,86 @@ class _FoodPageState extends State<FoodPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Image.asset(widget.food.imagePath),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            //food image
+            Image.asset(widget.food.imagePath),
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //food name
+                  Text(
+                    widget.food.name,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+
+                  //food price
+                  Text(
+                    'kshs ' + widget.food.price.toString() + '/=',
+                    style: TextStyle(
+                        fontSize: 16,
+                        color: Theme.of(context).colorScheme.primary),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  //food description
+                  Text(widget.food.description),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Add-ons",
+                    style: TextStyle(
+                        color: Theme.of(context).colorScheme.inversePrimary,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Divider(
+                    color: Theme.of(context).colorScheme.secondary,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+
+                  //addons
+                  Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                            color: Theme.of(context).colorScheme.secondary),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: ListView.builder(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        padding: EdgeInsets.zero,
+                        itemCount: widget.food.availableAddons.length,
+                        itemBuilder: (context, index) {
+                          Addon addon = widget.food.availableAddons[index];
+
+                          return CheckboxListTile(
+                            title: Text(addon.name),
+                            subtitle:
+                                Text('kshs ' + addon.price.toString() + '/='),
+                            value: false,
+                            onChanged: (value) {},
+                          );
+                        }),
+                  ),
+                ],
+              ),
+            ),
+            MyButton(onTap: () {}, text: "Add To Cart"),
+            const SizedBox(
+              height: 25,
+            ),
+          ],
+        ),
       ),
     );
   }
