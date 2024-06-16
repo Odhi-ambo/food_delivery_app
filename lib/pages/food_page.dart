861ed5124a0d0d32bb5796945fabaac20c9d1f23
Odhi-ambo <michaelderrick605@gmail.com>
2024-06-16 09:50:00 +0300

    initial commit

--- lib/pages/food_page.dart
+++ lib/pages/food_page.dart
@@ -18,92 +18,106 @@ class FoodPage extends StatefulWidget {
 class _FoodPageState extends State<FoodPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            //food image
-            Image.asset(widget.food.imagePath),
-            Padding(
-              padding: const EdgeInsets.all(25.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  //food name
-                  Text(
-                    widget.food.name,
-                    style: const TextStyle(
-                        fontWeight: FontWeight.bold, fontSize: 20),
-                  ),
+    return Stack(
+      children: [
+        Scaffold(
+          //appBar: AppBar(),
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                //food image
+                Image.asset(widget.food.imagePath),
+                Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      //food name
+                      Text(
+                        widget.food.name,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
 
-                  //food price
-                  Text(
-                    'kshs ${widget.food.price}/=',
-                    style: TextStyle(
-                        fontSize: 16,
-                        color: Theme.of(context).colorScheme.primary),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  //food description
-                  Text(widget.food.description),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    "Add-ons",
-                    style: TextStyle(
-                        color: Theme.of(context).colorScheme.inversePrimary,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Divider(
-                    color: Theme.of(context).colorScheme.secondary,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
+                      //food price
+                      Text(
+                        'kshs ${widget.food.price}/=',
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: Theme.of(context).colorScheme.primary),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      //food description
+                      Text(widget.food.description),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "Add-ons",
+                        style: TextStyle(
+                            color: Theme.of(context).colorScheme.inversePrimary,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Divider(
+                        color: Theme.of(context).colorScheme.secondary,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
 
-                  //addons
-                  Container(
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                            color: Theme.of(context).colorScheme.secondary),
-                        borderRadius: BorderRadius.circular(10)),
-                    child: ListView.builder(
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        padding: EdgeInsets.zero,
-                        itemCount: widget.food.availableAddons.length,
-                        itemBuilder: (context, index) {
-                          Addon addon = widget.food.availableAddons[index];
+                      //addons
+                      Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Theme.of(context).colorScheme.secondary),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: ListView.builder(
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            padding: EdgeInsets.zero,
+                            itemCount: widget.food.availableAddons.length,
+                            itemBuilder: (context, index) {
+                              Addon addon = widget.food.availableAddons[index];
 
-                          return CheckboxListTile(
-                            title: Text(addon.name),
-                            subtitle: Text('kshs ${addon.price}/='),
-                            value: widget.selectedAddons[addon],
-                            onChanged: (bool? value) {
-                              setState(() {
-                                widget.selectedAddons[addon] = value!;
-                              });
-                            },
-                          );
-                        }),
+                              return CheckboxListTile(
+                                title: Text(addon.name),
+                                subtitle: Text('kshs ${addon.price}/='),
+                                value: widget.selectedAddons[addon],
+                                onChanged: (bool? value) {
+                                  setState(() {
+                                    widget.selectedAddons[addon] = value!;
+                                  });
+                                },
+                              );
+                            }),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+                MyButton(onTap: () {}, text: "Add To Cart"),
+                const SizedBox(
+                  height: 25,
+                ),
+              ],
             ),
-            MyButton(onTap: () {}, text: "Add To Cart"),
-            const SizedBox(
-              height: 25,
-            ),
-          ],
+          ),
         ),
-      ),
+        SafeArea(
+          child: Container(
+            decoration:
+                BoxDecoration(color: Theme.of(context).colorScheme.secondary),
+            child: IconButton(
+                icon: Icon(Icons.arrow_back_ios_new_rounded),
+                onPressed: () => Navigator.pop(context)),
+          ),
+        )
+      ],
     );
   }
 }
