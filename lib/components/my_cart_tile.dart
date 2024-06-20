14f9a315e7e566283a179ae7f6489d263e694700
Odhi-ambo <michaelderrick605@gmail.com>
2024-06-20 10:14:49 +0300

    initial commit

--- lib/components/my_cart_tile.dart
+++ lib/components/my_cart_tile.dart
@@ -1,1 +1,12 @@
+import 'package:delivery_app/model/cart_item.dart';
+import 'package:flutter/material.dart';
 
+class MyCartTile extends StatelessWidget {
+  final CartItem cartItem;
+  const MyCartTile({super.key, required this.cartItem});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
