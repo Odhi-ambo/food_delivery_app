bbebc192767ce6381c7c40bc554a4fccc8352f2f
Odhi-ambo <michaelderrick605@gmail.com>
2024-06-20 10:27:23 +0300

    initial commit

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,3 +1,4 @@
+import 'package:delivery_app/components/my_cart_tile.dart';
 import 'package:delivery_app/model/restaurant.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -7,38 +8,40 @@ class CartPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<Restaurant>(builder: (context, restaurant, child) {
-      //cart
-      final userCart = restaurant.cart;
-      //scaffold UI
-      return Scaffold(
-        appBar: AppBar(
-          title: const Text('Cart'),
-          backgroundColor: Colors.transparent,
-          foregroundColor: Theme.of(context).colorScheme.inversePrimary,
-        ),
-        body: Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                itemCount: userCart.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
+    return Consumer<Restaurant>(
+      builder: (context, restaurant, child) {
+        //cart
+        final userCart = restaurant.cart;
+        //scaffold UI
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Cart'),
+            backgroundColor: Colors.transparent,
+            foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+          ),
+          body: Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  itemCount: userCart.length,
+                  itemBuilder: (context, index) {
+                    final cartItem = userCart[index];
+                    return MyCartTile(
+                        cartItem:
+                            cartItem); /*ListTile(
                     title: Text(userCart[index].food.name),
                     subtitle: Text(userCart[index].food.price.toString()),
                     trailing: IconButton(
                       icon: const Icon(Icons.delete),
                       onPressed: () {
-                        restaurant.removeFromCart(userCart[index]);
-                      },
-                    ),
-                  );
-                },
+                        restaurant.removeFromCart(userCart[index]);*/
+                  },
+                ),
               ),
-            ),
-          ],
-        ),
-      );
-    });
+            ],
+          ),
+        );
+      },
+    );
   }
 }
