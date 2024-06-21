d8ca641a01f7071a53bef7bfd75ced786a038be7
Odhi-ambo <michaelderrick605@gmail.com>
2024-06-21 09:01:09 +0300

    initial commit

--- lib/components/my_quantity_selector.dart
+++ lib/components/my_quantity_selector.dart
@@ -1,1 +1,20 @@
+import 'package:delivery_app/model/food.dart';
+import 'package:flutter/material.dart';
 
+class MyQuantitySelector extends StatelessWidget {
+  final int quantity;
+  final Food food;
+  final VoidCallback onIncrement;
+  final VoidCallback onDecrement;
+  const MyQuantitySelector(
+      {super.key,
+      required this.food,
+      required this.onDecrement,
+      required this.onIncrement,
+      required this.quantity});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
