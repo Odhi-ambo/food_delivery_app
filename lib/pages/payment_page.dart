f375aee517d280226690c172aa617487e3988369
Odhi-ambo <michaelderrick605@gmail.com>
2024-06-25 10:33:58 +0300

    Initial commit

--- lib/pages/payment_page.dart
+++ lib/pages/payment_page.dart
@@ -1,3 +1,4 @@
+import 'package:delivery_app/components/my_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_credit_card/flutter_credit_card.dart';
@@ -48,7 +49,15 @@ class _PaymentPageState extends State<PaymentPage> {
                   cvvCode = data.cvvCode;
                 });
               },
-              formKey: formKey)
+              formKey: formKey),
+          const Spacer(),
+          MyButton(
+            onTap: () {},
+            text: 'Pay Now',
+          ),
+          const SizedBox(
+            height: 25,
+          )
         ],
       ),
     );
