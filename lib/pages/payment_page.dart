c2ef574e23e92636b38b06169e1ecfaeea8daa25
Odhi-ambo <michaelderrick605@gmail.com>
2024-06-25 10:25:04 +0300

    Initial commit

--- lib/pages/payment_page.dart
+++ lib/pages/payment_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
 
 class PaymentPage extends StatefulWidget {
   const PaymentPage({super.key});
@@ -8,6 +9,12 @@ class PaymentPage extends StatefulWidget {
 }
 
 class _PaymentPageState extends State<PaymentPage> {
+  String cardNumber = '';
+  String expiryDate = '';
+  String cardHolderName = '';
+  String cvvCode = '';
+  bool isCvvFocused = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +22,25 @@ class _PaymentPageState extends State<PaymentPage> {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         title: const Center(child: Text('Payment')),
+      ),
+      body: Column(
+        children: [
+          CreditCardWidget(
+            cardNumber: cardNumber,
+            expiryDate: expiryDate,
+            cardHolderName: cardHolderName,
+            cvvCode: cvvCode,
+            showBackView: isCvvFocused,
+            onCreditCardWidgetChange: (p0) {},
+          ),
+          CreditCardForm(
+              cardNumber: cardNumber,
+              expiryDate: expiryDate,
+              cardHolderName: cardHolderName,
+              cvvCode: cvvCode,
+              onCreditCardModelChange: onCreditCardModelChange,
+              formKey: formKey)
+        ],
       ),
     );
   }
