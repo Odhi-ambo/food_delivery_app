d04972b68ffef1a1134fc0aa7cd7a2f39ba12f84
Odhi-ambo <michaelderrick605@gmail.com>
2024-06-26 10:07:57 +0300

    Initial commit

---
+++ lib/components/my_receipt.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyReceipt extends StatelessWidget {
+  const MyReceipt({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(padding: EdgeInsets.only(left: 25, right: 25, bottom: 25));
+  }
+}
