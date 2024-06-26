82db1a6eb486fd40a8d1cec9b9f9f81d04cdfca7
Odhi-ambo <michaelderrick605@gmail.com>
2024-06-26 10:14:57 +0300

    Initial commit

--- lib/components/my_receipt.dart
+++ lib/components/my_receipt.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class MyReceipt extends StatelessWidget {
   const MyReceipt({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.only(
+    return Padding(
+      padding: const EdgeInsets.only(
         left: 25,
         right: 25,
         bottom: 25,
       ),
       child: Column(
-        children: [Text('Thank you for your order!')],
+        children: [
+          const Text('Thank you for your order!'),
+          Container(
+            child: const Text("Receipt Here..."),
+          )
+        ],
       ),
     );
   }
