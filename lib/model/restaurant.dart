2500f7ec8d9b88597e8bf9904642b77d85e40fd1
Odhi-ambo <michaelderrick605@gmail.com>
2024-07-01 09:41:37 +0300

    Initial commit

--- lib/model/restaurant.dart
+++ lib/model/restaurant.dart
@@ -393,6 +393,7 @@ class Restaurant extends ChangeNotifier {
   String displayCartReceipt() {
     final receipt = StringBuffer();
     receipt.writeln("Here is your receipt.");
+    receipt.writeln("Date: $formattedDate");
     receipt.writeln();
 
     for (final cartItem in _cart) {
